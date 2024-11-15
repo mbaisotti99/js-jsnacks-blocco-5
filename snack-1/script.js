eea34a76f7c89a2ac3487162696dbf7fa0fd99fd
mbaisotti99 <matteo.baisotti@gmail.com>
2024-11-15 13:05:01 +0100

    feat:1+2

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,5 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach ((curItem) => {console.log(curItem);})
--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,10 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const result = []
+numbers.forEach((curItem) => {
+    result.push(++curItem)
+})
+
+console.table(result);
